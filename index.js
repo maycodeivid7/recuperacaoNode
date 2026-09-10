7fc7facfcad4d576fcf1ca464f904a857562f99b
Mayco Deivid <mayco.deivid@webjoi.com.br>
2026-09-09 22:05:41 -0300

    Smile Face :)

--- index.js
+++ index.js
@@ -41,5 +41,5 @@ app.post("/kc", (req, res) => {
 })
 
 app.listen(3000, () => {
-    console.log("Servidor escutando na porta 3000!");
+    console.log("Servidor escutando na porta 3000! :) ");
 })
