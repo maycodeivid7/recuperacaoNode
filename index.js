104f3a2eb0870d82472b8408e0c551f7e7084410
Mayco Deivid <mayco.deivid@webjoi.com.br>
2026-09-09 21:10:57 -0300

    Atividade de recuperação

---
+++ index.js
@@ -0,0 +1,51 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+// funções disponíveis
+app.get("/recursos", "req, res", () => {
+    
+    app.response("Você pode utilizar os endpoints: /recursos para listar todos os recursos, /cf converte Celsius para Fahrenheit, /fc converte Fahrenheit para Celsius, e /kc converte Celsius para Kelvin.")
+})
+
+// Celsius para Fahrenheit
+// F = (C x 9/5) + 32
+app.get("/cf", "req, res", () => {
+    const c = req.body.c;
+
+    const f = (c * (9/5)) + 32;
+
+    app.response( () =>  {
+        f;
+    })
+})
+
+// Fahrenheit para Celsius
+// C = (F - 32) x 5/9
+app.get("/fc", "req, res", () => {
+    const f = req.body.f;
+
+    const c = ((f - 32) * (5/9))
+    
+    app.response( () =>  {
+        c;
+    })
+})
+
+// Celsius para Kelvin
+// K = C + 273.15
+app.get("/kc", "req, res", () => {
+    const c = req.body.c;
+
+    const k = c + 273.15
+
+    app.response( () =>  {
+        k;
+    })
+})
+
+app.listen(3000, () => {
+    return {"mensagem" : "Servidor funcionando!"}
+})
